4223298aa7f778a49582753661b835838fd4d009
marcelohcortez <marcelohcortez@gmail.com>
2023-02-22 21:01:45 +0000

    .

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -5,13 +5,13 @@ import { getFirestore } from "@firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDrb5fnhyL5hac32HAsOObizqSorOcWbow',
-  authDomain: 'frontend-life-52f4a.firebaseapp.com',
-  projectId: 'frontend-life-52f4a',
-  storageBucket: 'frontend-life-52f4a.appspot.com',
-  messagingSenderId: '145346594344',
-  appId: '1:145346594344:web:db80cfbe411479ebff171d',
-  measurementId: 'G-7NELPLDCBN'
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 // Initialize Firebase
